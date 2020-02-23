847c48c4e67a3c73c49394ddf72b4b66cad45176
romam <Roman.maiboroda@innovecs.com>
2020-02-23 19:19:47 +0200

    Added express js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
-console.log(routes);
+const express = require('express');
 
-server.listen(7777);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('In the middleware2');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
