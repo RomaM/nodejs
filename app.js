4c79116b272f3c211c554cdc4757e0adfce91ccd
romam <Roman.maiboroda@innovecs.com>
2020-02-23 22:38:31 +0200

    Test data for express js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+app.use('/', (req, res, next) => {
     next();
 });
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
     console.log('In the middleware2');
+    res.send('<h1>Add Product</h1>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('In the middleware2');
+    res.send('<h1>Title</h1>');
+});
 
-server.listen(3000);
+
+app.listen(3000);
