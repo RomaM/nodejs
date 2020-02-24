9ab0560853a73de28f8c29c8b34e4df4d3ed01be
romam <Roman.maiboroda@innovecs.com>
2020-02-24 23:46:10 +0200

    Installed path module and changes for templates

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -15,7 +17,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found!</h1>')
+  res.status(404)
+      .sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
