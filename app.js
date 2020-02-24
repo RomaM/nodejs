32c77b75040b0bdf2da6ce4df6bc3c7123aa26bb
RomanMaiboroda <roman.maiboroda@innovecs.com>
2020-02-24 17:58:21 +0200

    added routes folder with a few routers

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware2');
-    res.send('<h1>Add Product</h1>');
-});
+// app.use('/', (req, res, next) => {
+//     next();
+// });
 
-app.use('/', (req, res, next) => {
-    console.log('In the middleware2');
-    res.send('<h1>Title</h1>');
-});
+app.use(express.urlencoded({extended: true}));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found!</h1>')
+});
 
 app.listen(3000);
