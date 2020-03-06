735a25694073c95259de0067763299860f3a4ee9
RomanMaiboroda <roman.maiboroda@innovecs.com>
2020-03-06 16:53:11 +0200

    Added a new template engine called handlebars

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const exphbs = require('express-handlebars');
+
 const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', exphbs({
+  extname: "hbs",
+  defaultLayout: "",
+  // layoutsDir: "",
+}));
+app.set('view engine', 'hbs');
+
 //app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -21,8 +29,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404)
-    .render('404', {pageTitle: '404'});
+  res.status(404).render('404', {pageTitle: '404'});
   // .sendFile(path.join(__dirname, 'views', '404.html'));
 
 });
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProds: products.length > 0});
 });
 
 module.exports = router;
