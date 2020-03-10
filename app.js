3a3b2ee81e281a81629affa74dd5af9eeb838cce
RomanMaiboroda <roman.maiboroda@innovecs.com>
2020-03-10 17:58:40 +0200

    ejs templates changes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+// Default configuration to setting views folder
 //app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -25,14 +26,14 @@ const shopRoutes = require('./routes/shop');
 //     next();
 // });
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: '404'});
+  res.status(404).render('404', {pageTitle: '404', path: '404'});
   // .sendFile(path.join(__dirname, 'views', '404.html'));
 
 });
